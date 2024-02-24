a173a5dd4b1fa3fa255b4f2ff159221e5c1fc287
Melanie Payne <melanie_payne@outlook.com>
2024-02-24 16:29:12 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,27 @@ router.get("/home", function (req, res, next) {
 });
 
 router.get("/about_me", function (req, res, next) {
-  res.render("about_me", { title: "about mel" });
+  res.render("about_me", { title: "melanie codes" });
 });
 
 router.get("/projects", function (req, res, next) {
-  res.render("projects", { title: "projects" });
+  res.render("projects", { title: "melanie codes" });
 });
 
 router.get("/services", function (req, res, next) {
-  res.render("services", { title: "services" });
+  res.render("services", { title: "melanie codes" });
 });
 
 router.get("/contact_me", function (req, res, next) {
-  res.render("contact_me", { title: "contact me" });
+  res.render("contact_me", { title: "melanie codes" });
 });
 
 router.get("/blog", function (req, res, next) {
-  res.render("blog", { title: "blog" });
+  res.render("blog", { title: "melanie codes" });
 });
 
 router.get("/welcome-to-my-blog", function (req, res, next) {
-  res.render("welcome-to-my-blog", { title: "welcome to my blog" });
+  res.render("welcome-to-my-blog", { title: "melanie codes" });
 });
 
 module.exports = router;
